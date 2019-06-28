d0aa4f3c9f79424a4bdfd2c0a693a788d7520fee
thangbq190893 <46780842+thangbq190893@users.noreply.github.com>
2019-06-28 09:12:43 +0700

    angular-admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LoginComponent} from './login/login/login.component';
+import {DasboardComponent} from './dashboard/dasboard/dasboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
